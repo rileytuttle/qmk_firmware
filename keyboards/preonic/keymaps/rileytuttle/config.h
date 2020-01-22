37b0b5ea311d08dc94c3345c0951eb56a1d237b5
Riley Tuttle <rileytuttle@gmail.com>
2020-01-21 19:13:01 -0500

    still trying to fix the music mode stuff

--- keyboards/preonic/keymaps/rileytuttle/config.h
+++ keyboards/preonic/keymaps/rileytuttle/config.h
@@ -8,7 +8,6 @@
                                   SONG(COLEMAK_SOUND), \
                                   SONG(DVORAK_SOUND) \
                                 }
-    #define AUDIO_CLICKY                                
 #endif
 
 #define MUSIC_MASK (keycode != KC_NO)
