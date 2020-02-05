9888bb35cb78bf4bb281f4e6fe75e3e71e149579
Riley Tuttle <rileytuttle@gmail.com>
2020-02-04 20:31:48 -0500

    removing music print statement as we have solved the music stuff

--- quantum/process_keycode/process_music.c
+++ quantum/process_keycode/process_music.c
@@ -101,7 +101,6 @@ void music_all_notes_off(void) {
 }
 
 bool process_music(uint16_t keycode, keyrecord_t *record) {
-    dprintf("inside process music, keycode = %d\n", keycode);
     if (keycode == MU_ON && record->event.pressed) {
         music_on();
         return false;
