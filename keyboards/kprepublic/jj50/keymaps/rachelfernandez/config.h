ce62cb5bb8d51f851a596649fc0c3b24e8841bbf
Riley Tuttle <rileytuttle@gmail.com>
2021-12-19 21:46:08 -0500

    adding a layer for kel. still figuring out how to get the board into bootloader mode

---
+++ keyboards/kprepublic/jj50/keymaps/rachelfernandez/config.h
@@ -0,0 +1,25 @@
+/* Copyright 2015-2021 Jack Humbert
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_DELAY 10
+#define MOUSEKEY_INTERVAL 10
+#define MOUSEKEY_MOVE_DELTA 1
+#define MOUSEKEY_INTIAL_SPEED 15
+#define MOUSEKEY_BASE_SPEED 500
+
---
+++ keyboards/kprepublic/jj50/keymaps/rachelfernandez/security.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define PERSONAL_PWORD "personal_testing.123!"
+// #define WORK_PWORD "CRomulon1119.zinc"
+#define WORK_PWORD "some_password" // starting 12/21
