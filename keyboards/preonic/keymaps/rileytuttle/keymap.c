fe2841461ccbb9299783deab14a5f97e30b5423c
Riley Tuttle <rileytuttle@gmail.com>
2022-11-21 09:41:24 -0500

    changing some game layer keys

--- keyboards/preonic/keymaps/rileytuttle/keymap.c
+++ keyboards/preonic/keymaps/rileytuttle/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |RGBKEY| Alt  | GUI  | Space| LOWER| Space| RAISE| Home |PageDn|PageUp| End  |
+ * | Ctrl |  `   | Alt  | GUI  | Space| LOWER| Space| RAISE| Home |PageDn|PageUp|QWERTY|
  * `-----------------------------------------------------------------------------------'
  */
  
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_ENT,  \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LCTL, RGBKEY,  KC_LALT, KC_LGUI, KC_SPC,  LOWER,   KC_SPC,  RAISE,   KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY  \
+  KC_LCTL, KC_GRV,  KC_LALT, KC_LGUI, KC_SPC,  LOWER,   KC_SPC,  RAISE,   KC_MNXT, KC_VOLD, KC_VOLU, DF(_QWERTY) \
 ),
 
 /* LALT
